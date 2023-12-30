13d312993b71faadd3b6cc09ecf6b16fecbae97e
Sushant Umap <sushantumap1234@gmail.com>
2023-12-30 23:28:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./assets/css/style.css";
+
+import Menu from "./components/Menu";
+import Products from "./components/Products";
+
+
+const App = () => {
+  return (
+    <>
+      
+      <Menu />
+      <Products />
+      
+    </>
+  );
+};
+
+export default App;
